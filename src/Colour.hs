15a916d991ec98d9bdf6f1fd6ca8fc918dffb5c7
OriReschini <orireschini@gmail.com>
2022-02-01 00:32:05 -0300

    Reorganized Colour module.

--- src/Colour.hs
+++ src/Colour.hs
@@ -8,7 +8,7 @@ import Data.GraphViz.Attributes.Complete (Attribute(Label, Color), toColorList, 
 import Data.GraphViz.Attributes          (toLabel, Labellable)
 import Data.GraphViz.Printing            (renderDot, toDot)
 import Data.Text.Lazy                    (unpack)
-import Data.Graph.Inductive.Graph        (mkGraph, LNode, LEdge, labNodes, labEdges, neighbors, nodes, Node)
+import Data.Graph.Inductive.Graph        (mkGraph, LEdge, labNodes, labEdges, neighbors, nodes, Node)
 import Data.List                         (sortOn, sort)
 import Draw
 import Lib
@@ -25,12 +25,8 @@ params = Params { isDirected       = False
                        }
                     where nodes (num, (label, colour)) = [toLabel label, colour]
 
---colorsParams = params { fmtNode = \(a, (label, colour)) -> color a label colour}
---colorsParams = params { fmtNode = \(a, label) -> color a label (Color $ toColorList [RGB 40 255 40]) }
-
 color a label colour | a == 2 = [toLabel label, colour]
                      | otherwise = [toLabel label, colour]
---labelledNodesParams = myDefaultParams { fmtNode= \(_,label)-> [Label (StrLabel (pack label)), Color $ toColorList [RGB 40 255 40] ] }  
 
 -- hasLoop l returns true if there is a loop in the list l of edges. If not, it returns false
 hasLoop :: [LEdge ()] -> Bool
@@ -39,22 +35,21 @@ hasLoop ( (ori, des, ()):es )
     | ori == des = True
     | otherwise  = hasLoop es 
 
--- given a graph, sortGraphNodes returns a list ordered by the second component in descending order with tuples
--- where first component is Int representing the node and second component is a list of nodes(their ints) that 
--- are connected to the first component
---sortGraphNodes :: [LNode String] -> [LEdge ()] -> [(Int, [Int])] -> [(Int, [Int])]
+-- given a graph, sortGraphNodes returns a list of tuples where the first component of each element is a Node (represented by an Int)
+-- and the second component is a list of Nodes that are connected to the Node in the first component. Also, the list of tuples is 
+-- ordered by the length of the list in the second component in descending order.
 sortGraphNodes :: Gr String () -> [(Node, [Node])]
 -- negate (length neighbours) is used so that it is sorted in a descending order
 sortGraphNodes g = sortOn (\(node, neighbours) -> negate(length neighbours)) nodesWithNeighbours
                     where nodesWithNeighbours = map (\i -> (i, neighbors g i)) (nodes g)
 
 availableColours :: [ColorList]
--- orden colores: rojo, verde, azul, celeste, fucsia, naranja, violeta, amarillo, azul no tan oscuro, verde oscuro, azul oscuro, marron
+-- colours: red, green, blue, light blue, fuchsia, orange, purple, yellow, light pink, dark green, dark blue, brown
 availableColours = [toColorList [RGB 255 0 0], toColorList [RGB 0 255 0], toColorList [RGB 0 0 255], toColorList [RGB 0 255 255],
                     toColorList [RGB 255 0 255], toColorList [RGB 255 128 0], toColorList [RGB 127 0 255], toColorList [RGB 255 255 0], 
-                    toColorList [RGB 0 128 255], toColorList [RGB 0 102 0], toColorList [RGB 0 0 102], toColorList [RGB 102 0 0]]
+                    toColorList [RGB 255 190 190], toColorList [RGB 0 102 0], toColorList [RGB 0 0 102], toColorList [RGB 102 0 0]]
 
--- retrurns a list of tuples (n, i) where n is a node and i is the int that represents the colour assigned
+-- returns a list of tuples (n, colour) where n is a node and colour represents the colour assigned
 realAssignment :: [(Node, [Node])] -> [(Node, ColorList)] -> Int -> [(Node, ColorList)]
 realAssignment [] res _ = res
 realAssignment sortedNodes res k = realAssignment [n | n <- sortedNodes, (fst n, availableColours !! k) `notElem` newRes] newRes (k+1)
@@ -68,30 +63,11 @@ realAssignment sortedNodes res k = realAssignment [n | n <- sortedNodes, (fst n,
 
 -- assignColours g returns a coloured graph
 assignColours :: Gr String () -> Gr (String, Attribute) ()
---assignColours g = mkGraph newNodes edges
--- para definir newnodes llamo a realassignment y ahi tengo que ver como genero los colores y despues asigno
-    
---    assignColoursAux sortedNodes [] 1
-                    --where sortedNodes = sortGraphNodes g 
-                          --assignColoursAux [] res _ = res
-                          --assignColoursAux sn res k = let newRes = colourNodesWith k sn res
-                                                        --in assignColoursAux [n | n <- sn, (fst n, k) `notElem` newRes] newRes (k+1)
-                          --colourNodesWith _ [] res = res
-                          -- if node has a neighbour with colour k
-                          --colourNodesWith k ((node, neigh) : ns) res = if any (\n -> (n, k) `elem` res) neigh
-                                                                        --then colourNodesWith k ns res
-                                                                        --else colourNodesWith k ns ((node, k) : res)
---assignColours g = mkGraph newNodes edges
---                    where edges = labEdges g
---                          newNodes = map (\(a,lab)->(a,(lab,Color $ (availableColours !! 5))) ) (labNodes g)
 assignColours g = mkGraph newNodes (labEdges g)
                     where sortedNodes = sortGraphNodes g  -- sortedNodes :: [(Node, [Node])] 
                           nodesWithColours = sort $ realAssignment sortedNodes [] 0 -- nodesWithColours :: [(Node, ColorList)]
                           nodesWithLabels = sort $ labNodes g
                           newNodes = zipWith (\(n1, lab) (n2, colour) -> (n1, (lab, Color $ colour)) ) nodesWithLabels nodesWithColours
--- tengo nodesWithColours y (labNodes g), tengo que combinar para conseguir las labels de (labNodes g)
---                        newNodes = map ()
-
 
 drawWithCol :: MonadTrans t => Gr String () -> Name -> t IO ()
 drawWithCol g name = do
@@ -99,16 +75,10 @@ drawWithCol g name = do
     let dot = unpack $ renderDot $ toDot $ graphToDot params graphColoured
     lift (createFile dot name)
 
--- VER CASO DE SI ES GRAFO VACÍO (0 NODOS, 0 ARISTAS)
-
--- dado un grafo y un string que representa el nombre
+-- given a graph g and a name n, this function colours g and creates a file called n with the coloured graph 
 colour :: (MonadTrans t, MonadError (t IO)) => Gr String () -> Name -> t IO ()
 colour g name = --drawWithCol g name
     do 
         let edges = labEdges g 
         if hasLoop edges then throwUncolourableG name else drawWithCol g name
     
-    --do
-    --let dot = unpack $ renderDot $ toDot $ graphToDot colorsParams g
-    --lift (createFile dot name)
-    --lift (removeFile (name++".dot"))
